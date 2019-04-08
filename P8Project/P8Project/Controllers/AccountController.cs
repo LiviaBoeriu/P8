6e6e838f5dc99a0583690a4f7d7bb796d41b3e7b
Livia Boeriu <boeriulivia@gmail.com>
2019-04-08 11:36:06 +0200

    Made the code for the karen stuff in the database

---
+++ P8Project/P8Project/Controllers/AccountController.cs
@@ -0,0 +1,70 @@
+﻿using P8Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace P8Project.Controllers
+{
+    
+
+    public class AccountController : Controller
+    {
+        private DiceItUpEntities2 db = new DiceItUpEntities2();
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Profile(PlayerProfile profile)
+        {
+            return View(profile);
+        }
+        
+
+        // GET: account/login
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PlayerProfile playerProfile = db.PlayerProfiles.Find(id);
+            if (playerProfile == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.location_id = new SelectList(db.Locations, "location_id", "zip_code", playerProfile.location_id);
+            ViewBag.player_id = new SelectList(db.PlayerLogins, "player_id", "email", playerProfile.player_id);
+            ViewBag.profile_level = new SelectList(db.ProfileTitles, "profile_level", "title", playerProfile.profile_level);
+            return View(playerProfile);
+        }
+
+        // POST: account/login
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login([Bind(Include = "email , password")] PlayerLogin playerLogin)
+        {
+            if (ModelState.IsValid)
+            {
+                var userLogin = db.PlayerLogins.FirstOrDefault(row => row.email == playerLogin.email && row.password == playerLogin.password);
+                if (userLogin != null) {
+                    var userProfile = db.PlayerProfiles.FirstOrDefault(row => row.player_id == userLogin.player_id);
+                    return RedirectToAction("Profile", userProfile);
+                }
+                return View();
+            }
+           return View();
+        }
+            //ViewBag.location_id = new SelectList(db.Locations, "location_id", "zip_code", playerProfile.location_id);
+            //ViewBag.player_id = new SelectList(db.PlayerLogins, "player_id", "email", playerProfile.player_id);
+            //ViewBag.profile_level = new SelectList(db.ProfileTitles, "profile_level", "title", playerProfile.profile_level);
+            //return Redirect("Account/Profile");
+    }
+}
